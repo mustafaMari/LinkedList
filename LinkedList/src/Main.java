27b79caf2bf09d46ef83d5861ff1d370708d2cc2
mustafaMari <61988029+mustafaMari@users.noreply.github.com>
2021-02-04 18:41:49 +0100

    TwoWayIncluded

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -7,11 +7,29 @@ public class Main {
         linkedList.add(56);
         linkedList.add(3);
         linkedList.add(2);
-
         System.out.println(linkedList);
-//        linkedList = linkedList.reverse(linkedList.sentinel);
-//        linkedList.sentinel = linkedList.reverse(linkedList.sentinel);
         linkedList.reverse();
         System.out.println(linkedList);
+        TwoWayLinkedListWithHead<Integer> twoWayLinkedListWithHead = new TwoWayLinkedListWithHead<>();
+        twoWayLinkedListWithHead.add(45);
+        twoWayLinkedListWithHead.add(44);
+        twoWayLinkedListWithHead.add(43);
+        twoWayLinkedListWithHead.add(42);
+        twoWayLinkedListWithHead.add(41);
+        twoWayLinkedListWithHead.add(40);
+        System.out.println(twoWayLinkedListWithHead);
+        twoWayLinkedListWithHead.toStringReverse();
+        System.out.println(twoWayLinkedListWithHead.toStringReverse());
+        TwoWayLinkedListWithHead<Integer> twoWayLinkedListWithHead1 = new TwoWayLinkedListWithHead<>();
+        twoWayLinkedListWithHead1.add(4);
+        twoWayLinkedListWithHead1.add(5);
+        twoWayLinkedListWithHead1.add(6);
+        twoWayLinkedListWithHead1.add(7);
+        twoWayLinkedListWithHead1.add(8);
+        twoWayLinkedListWithHead1.add(9);
+        twoWayLinkedListWithHead1.add(10);
+        twoWayLinkedListWithHead.add(twoWayLinkedListWithHead1);
+        System.out.println(twoWayLinkedListWithHead);
+
     }
 }
